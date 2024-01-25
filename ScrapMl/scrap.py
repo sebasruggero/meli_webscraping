a815bdcd44925c3c1ca33650553c3ffd14a4cd2f
sebasruggero <ruggerosebastian@gmail.com>
2024-01-25 02:21:53 -0300

    Mensaje descriptivo de los cambios realizados

--- ScrapMl/scrap.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = 'https://listado.mercadolibre.com.ar/continental#D[A:continental]'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Ahora puedes buscar elementos y extraer información usando soup.find() o soup.select()
