2c6100b77b82d7ca6ed6ff5af5354c0a7c9d2f07
Sebastian Ruggero <ruggerosebastian@gmail.com>
2024-01-30 22:10:00 -0300

    se corrigio el append
    Se corrigio el codigo del append

--- ScrapMl/scriptAppend.py
+++ ScrapMl/scriptAppend.py
@@ -10,9 +10,9 @@ dfs = []
 # Iterar sobre los archivos en la carpeta
 for archivo in os.listdir(carpeta):
     if archivo.endswith('.csv'):
-        # Leer el archivo CSV
+        # Leer el archivo CSV y especificar el tipo de datos de todas las columnas como cadena
         ruta_archivo = os.path.join(carpeta, archivo)
-        df = pd.read_csv(ruta_archivo)
+        df = pd.read_csv(ruta_archivo, dtype=str)  # Todos los datos como cadena
 
         # Verificar duplicados en la columna 'Publicacion'
         duplicados = df[df.duplicated('Publicacion')]
@@ -27,8 +27,16 @@ for archivo in os.listdir(carpeta):
         # Agregar el DataFrame limpio a la lista
         dfs.append(df)
 
+# Configurar opciones de formato
+pd.set_option('display.float_format', '{:.3f}'.format)
+pd.set_option('display.precision', 3)  # Controlar la precisión al imprimir
+
 # Consolidar todos los DataFrames en uno
 df_consolidado = pd.concat(dfs, ignore_index=True)
+
+# Restaurar configuración predeterminada después de la concatenación
+pd.reset_option('display.float_format')
+pd.reset_option('display.precision')
 
 # Guardar el DataFrame consolidado en un nuevo archivo CSV
 df_consolidado.to_csv('consolidado.csv', index=False)
